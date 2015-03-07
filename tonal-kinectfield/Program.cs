0fba07054c4d9b74d01b67bea6169aadcfb67825
Scott Walters <srwalte2@illinois.edu>
2015-03-06 18:55:26 -0600

    Hello world with Kinect library import

---
+++ tonal-kinectfield/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Kinect;
+
+namespace tonal_kinectfield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello world!");
+        }
+    }
+}
